971ffc425bdc4d95882a4ac2201b1f8ca3c2467f
ecqiu <eddy.c.qiu@gmail.com>
2021-12-07 19:47:56 +1100

    q7

---
+++ code/q07.R
@@ -0,0 +1,17 @@
+library(data.table)
+
+test=unlist(fread('/home/eqiu/code_projects/aoc_2021/data/input_7',fill=T))
+
+
+
+
+#7.1
+sum(abs(test-median(test)))
+
+#7.2
+gauss=function(x){
+  return(x*(x+1)/2)
+}
+sum(gauss(abs(test-round(mean(test))-1)))
+#bit of a guess but the n^2 term should dominate, so check round(mean(test)) and numbers around it
+
